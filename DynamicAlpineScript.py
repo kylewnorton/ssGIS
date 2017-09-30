0d1a0bc793c2ba484b878fc0e2cb2244294a4a3c
Kyle Norton <kylewnorton@gmail.com>
2017-09-30 15:44:12 -0600

    iterate the buffer Tool!!!!!

--- DynamicAlpineScript.py
+++ DynamicAlpineScript.py
@@ -18,12 +18,90 @@ for field in fieldDeletion:
     arcpy.management.DeleteField("Alpine", field)
 
 radius = .1
-while radius < .5:
+while radius < .9:
+    # Prep environments
+    featureClassesForDeletion = ["LeftOver", "facCapture", "BufferUnion", "AlpineBuffer", "sumStats", "Sum_Stats1"]
+    for thingVariable in featureClassesForDeletion:
+        arcpy.management.Delete(thingVariable, None)
+
+    fieldDeletion = ["NetSF", "NetSF95Occ"]
+    for field in fieldDeletion:
+        arcpy.management.DeleteField("Alpine", field)
+
+    # Add a Numeric Field for the facility then use the calculate tool to get a numeric value in the field
+    fieldName = ["NetSF", "NetSF95Occ"]
+    for fieldNameIndex in fieldName:
+        facility = "Alpine"
+        fieldPrecision = 15
+        arcpy.AddField_management(facility, fieldNameIndex, "DOUBLE", fieldPrecision)
+
+    # I know there is a way to incorporate this into the for loop above...but I am unsure on how to do it.
+    arcpy.management.CalculateField(facility, "NetSF", '"250,466.00"', "PYTHON_9.3", None) #interesting syntax for the 250,466!!!!!!!!!!!!!!!!!!!!!!!
+    arcpy.management.CalculateField(facility, "NetSF95Occ", "!NetSF! * .95", "PYTHON_9.3", None)
+
+
     # the buffer tool
     facility = "Alpine"
     facBufferName = "AlpineBuffer"
+
     # Run Buffer using the variables set above and pass the remaining
     # parameters in as strings
-    arcpy.Buffer_analysis(facility, facBufferName, radius, "Miles", "FULL", "ROUND", "NONE")
-    # arcpy.analysis.Buffer("Alpine", r"C:\GISData\Utah\Salt Lake County\Alpine AntiMatter Test\New Alpine AntiMatter Test.gdb\Buffer", ".1 Miles", "FULL", "ROUND", "NONE", None, "PLANAR")
+    arcpy.Buffer_analysis(facility, facBufferName, str(radius) + " Miles", "FULL", "ROUND", "NONE")
+
+    # union tool
+    inFeatures = [facBufferName, "SLCoTractsDiv"]
+    bufferUnion = "BufferUnion"
+
+    arcpy.Union_analysis (inFeatures, bufferUnion)
+
+
+    # Make a layer tool uses the field info tool to get ratios for splitting demographics
+    # MakeFeatureLayer_management (in_features, out_layer, {where_clause}, {workspace}, {field_info})
+    outPutInsideLayer = "facCapture"
+    outPutOutsideLayer = "LeftOver"
+    arcpy.MakeFeatureLayer_management (bufferUnion, outPutInsideLayer, "FID_AlpineBuffer = 1") #I think I could use a dictionary here
+    arcpy.MakeFeatureLayer_management (bufferUnion, outPutOutsideLayer, "FID_AlpineBuffer = -1") #I think I could use a dictionary here
+
+    nameOfSumTable = "sumStats"
+    fieldToSum = "why_csv_Total_population"
+    # sum up the people located in the buffer shown in feature class F1Capture
+    arcpy.Statistics_analysis(outPutInsideLayer, nameOfSumTable, [[fieldToSum, "SUM"]])
+
+    # times the Sum by the SF demand per person
+    # First, Add the field to the sum table
+    # Set local variables
+    fieldName = ["SFDemanded", "NetSF", "NetSF95Occ"]
+    for fieldNameIndex in fieldName:
+        inFeatures = "sumStats"
+        fieldPrecision = 15
+        arcpy.AddField_management(inFeatures, fieldNameIndex, "DOUBLE", fieldPrecision)
+
+    # Times the TotPop times the Utah SLC 7.93/per person
+    # Set local variable(s)
+    Multiplier = 7.93
+    tableToCalc = "sumStats"
+    fieldToCalc = "SFDemanded"
+    capturedPopField = "!SUM_why_csv_Total_population!"
+    arcpy.management.CalculateField(tableToCalc, fieldToCalc, "!SUM_why_csv_Total_population! * 7.93", "PYTHON_9.3", None)
+
+    #**************************************************************
+    fc = "Alpine"
+    field = "NetSF"
+    cursor = arcpy.SearchCursor(fc)
+
+    # print (cursor)
+
+    for rowsupply in cursor:
+        suppliedsf = (rowsupply.getValue(field))
+
+    fc = "sumStats"
+    field = "SFDemanded"
+    cursor = arcpy.SearchCursor(fc)
+    for rowdemand in cursor:
+        demandedsf = (rowdemand.getValue(field))
+
+    if demandedsf>suppliedsf:
+        print ("Yes")
+    else:
+        print("No")
     radius += .1
